b65018738357f874f2859bfb86c6c4cf6a61cb75
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-09 20:17:51 +0100

    create theme.js

---
+++ src/core/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    colors: {
+        white: "#FFF",
+        black: "#000",
+        blue: "#007aff",
+        lightTeal: "#007aff",
+        lightPink: "#FFD4D7",
+        pink: "#54595F",
+        lightBlack: "#54595F",
+        grey: "#7A7A7A",
+        superLightGrey: "#666",
+        lightGrey: "#e6e6e6",
+    },
+    breakpoints: {
+        Tablet: "1030", Mobile: "780",
+    },
+};
