b8c994da2b6ce5e90f580b576d9acfea6bce8399
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-09 20:14:49 +0100

    added themeProvider, Header in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import {Header} from "./features/header/Header/styled";
+import {ThemeProvider} from "styled-components";
+import {theme} from "./core/theme";
 
 function App() {
-  return (
-    <>ananan</>
-  );
+    return (<ThemeProvider theme={theme}>
+            <>
+                <Header/>
+                tananan
+            </>
+        </ThemeProvider>
+    );
 }
 
 export default App;
