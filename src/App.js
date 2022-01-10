fa305b0cf54ffa725aed06d8459b3d3fdf06be36
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-10 22:50:56 +0100

    added ProductTiles in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {Header} from "./features/header/Header/styled";
-import {ThemeProvider} from "styled-components";
-import {theme} from "./core/theme";
+import { Header } from "./features/header/Header/index";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
+import { ProductsTiles } from "./common/products/ProductsTiles";
 
 function App() {
-    return (<ThemeProvider theme={theme}>
-            <>
-                <Header/>
-                tananan
-            </>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <ProductsTiles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
