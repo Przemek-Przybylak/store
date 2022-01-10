6586807257590edba8afccf798137fa9a3d9afbd
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-10 22:57:35 +0100

    added more styles to HeaderWrapper

--- src/features/header/Header/styled.js
+++ src/features/header/Header/styled.js
@@ -1,5 +1,8 @@
-export const Header = () => {
-    return (
-        <>nana</>
-    )
-}
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+  padding: 0 30px;
+  margin: 0;
+  height: 60px;
+  background-color: ${({ theme }) => theme.colors.black};
+`;
