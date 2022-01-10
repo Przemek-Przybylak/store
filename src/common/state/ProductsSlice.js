404f48327b26a9db5d6b44d8bba7a90ce992f894
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-10 22:55:29 +0100

    create ProductSlice.js

---
+++ src/common/state/ProductsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState: {
+    allProducts: [],
+    status: "",
+  },
+  reducers: {
+    fetchAllProducts: (state) => {
+      state.status = "loading";
+    },
+    fetchAllProductsSuccess: (state, { payload: allProducts }) => {
+      state.allProducts = allProducts;
+      state.status = "success";
+    },
+    setApplicationStatus: (state, { payload }) => {
+      state.status = payload;
+    },
+  },
+});
+
+export const {
+  fetchAllProducts,
+  fetchAllProductsSuccess,
+  setApplicationStatus,
+} = productsSlice.actions;
+
+export const selectAllProducts = (state) => state.products.allProducts;
+export const selectStatus = (state) => state.products.status;
+
+export default productsSlice.reducer;
