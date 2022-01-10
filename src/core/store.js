a75c1db4e9beba18856065c5972c3178d5925a33
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-10 22:56:32 +0100

    added productReducer in store

--- src/core/store.js
+++ src/core/store.js
@@ -1,14 +1,17 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import rootSaga from "./rootSaga";
 import createSagaMiddleware from "redux-saga";
+import productsReducer from "../common/state/ProductsSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-    reducer: {},
-    middleware: [sagaMiddleware],
+  reducer: {
+    products: productsReducer,
+  },
+  middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(rootSaga());
+sagaMiddleware.run(rootSaga);
 
 export default store;
