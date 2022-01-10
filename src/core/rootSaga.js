208e26bf74c10c8558178d1f9a944f02165bad0d
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-10 22:56:07 +0100

    added watchProducts saga in root saga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,5 +1,6 @@
 import { all } from "@redux-saga/core/effects";
+import { watchProducts } from "../common/state/ProductsSaga";
 
 export default function* rootSaga() {
-    yield all();
+  yield all([watchProducts()]);
 }
