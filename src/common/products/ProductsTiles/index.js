3d010ca4c6a1a2f1e241c2f7ce4bb0ef3a121e7b
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-11 23:26:43 +0100

    remove dispatch fetchAllProducts from ProductTiles

--- src/common/products/ProductsTiles/index.js
+++ src/common/products/ProductsTiles/index.js
@@ -1,14 +1,3 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAllProducts, selectAllProducts } from "../../state/ProductsSlice";
-
 export const ProductsTiles = () => {
-  const dispatch = useDispatch();
-  const allProducts = useSelector(selectAllProducts);
-
-  useEffect(() => {
-    dispatch(fetchAllProducts());
-  }, [dispatch]);
-  console.log(allProducts);
   return <>nanana</>;
 };
