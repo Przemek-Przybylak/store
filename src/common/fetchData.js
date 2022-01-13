ba9826a575c709fe20441b58d2a121643db83d7a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-13 21:29:40 +0100

    transferred FetchData | change path

---
+++ src/common/fetchData.js
@@ -0,0 +1,8 @@
+export const fetchData = async () => {
+  const response = await fetch(`https://fakestoreapi.com/products/`);
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
+};
