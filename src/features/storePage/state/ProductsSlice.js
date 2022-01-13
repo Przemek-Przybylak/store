20cb9ad09db7882502a5d5a8c0d6db488ba9852a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-13 21:34:45 +0100

    adapting ProductsSlice to use generatorSlice

--- src/features/storePage/state/ProductsSlice.js
+++ src/features/storePage/state/ProductsSlice.js
@@ -1,33 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { generatorSlice } from "../../../common/generatorSlice";
 
-const productsSlice = createSlice({
+export const productsSlice = generatorSlice({
   name: "products",
-  initialState: {
-    allProducts: [],
-    allCategories: [],
-    status: "",
-  },
-  reducers: {
-    fetchAllProducts: (state) => {
-      state.status = "loading";
-    },
-    fetchAllProductsSuccess: (state, { payload: allProducts }) => {
-      state.allProducts = allProducts;
-      state.status = "success";
-    },
-    setApplicationStatus: (state, { payload }) => {
-      state.status = payload;
-    },
-  },
 });
 
-export const {
-  fetchAllProducts,
-  fetchAllProductsSuccess,
-  setApplicationStatus,
-} = productsSlice.actions;
-
-export const selectAllProducts = (state) => state.products.allProducts;
-export const selectStatus = (state) => state.products.status;
+export const { actions, selectors } = productsSlice;
 
 export default productsSlice.reducer;
