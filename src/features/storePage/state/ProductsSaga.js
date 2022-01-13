d473c09aa1ad8cdafea3ef8a9ddeb7c28e41f299
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-13 21:33:57 +0100

    adapting ProductsSaga to use generatorSaga

--- src/features/storePage/state/ProductsSaga.js
+++ src/features/storePage/state/ProductsSaga.js
@@ -1,29 +1,8 @@
-import { call, delay, put, takeLatest } from "redux-saga/effects";
-import {
-  fetchAllProducts,
-  setApplicationStatus,
-  fetchAllProductsSuccess,
-} from "./ProductsSlice";
-import { fetchData } from "../fetchData";
+import { listSaga } from "../../../common/generatorSaga";
+import { actions } from "./ProductsSlice";
 
-function* fetchProductsHandler() {
-  try {
-    delay(1000);
-    const allProducts = yield call(fetchData);
-    yield put(fetchAllProductsSuccess(allProducts));
-  } catch (error) {
-    yield put(setApplicationStatus("error"));
-  }
-}
-
-function* fetchCategoriesHandler() {
-  try {
-    const allCategories = yield call(fetchData(categories));
-    yield put()
-  }
-}
-
-export function* watchProducts() {
-  yield takeLatest(fetchAllProducts.type, fetchCategoriesHandler)
-  yield takeLatest(fetchAllProducts.type, fetchProductsHandler);
+export function* watchFetchProducts() {
+  yield listSaga({
+    actions,
+  });
 }
