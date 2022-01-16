0d3915e0d8e48d77f27dbc739f12ab5e61d2c61d
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-16 22:22:46 +0100

    added status in StorePage

--- src/features/storePage/StorePage/index.js
+++ src/features/storePage/StorePage/index.js
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../../../common/fetchData";
 import { ProductsTiles } from "../../../common/products/ProductsTiles";
 import { Header } from "../../header/header/Header";
 import { actions, selectors } from "../state/ProductsSlice";
 
 export const StorePage = () => {
   const dispatch = useDispatch();
-  const allProducts = useSelector(selectors.selectDetails);
+  const allProducts = useSelector(selectors.selectProducts);
+  const selectStatus = useSelector(selectors.selectStatus);
+
+  console.log(selectStatus);
 
   useEffect(() => {
     dispatch(actions.fetch("products"));
@@ -14,7 +18,7 @@ export const StorePage = () => {
   return (
     <>
       <Header />
-      <ProductsTiles products={allProducts} />
+      {allProducts && <ProductsTiles allProducts={allProducts} />}
     </>
   );
 };
