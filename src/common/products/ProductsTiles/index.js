864a298479a1319ba27c53481e91f409c805472e
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-16 22:19:28 +0100

    added some prop value in ProductsTiles

--- src/common/products/ProductsTiles/index.js
+++ src/common/products/ProductsTiles/index.js
@@ -1,3 +1,16 @@
-export const ProductsTiles = () => {
-  return <>nanana</>;
+export const ProductsTiles = ({ allProducts }) => {
+  console.log(allProducts);
+  return (
+    <>
+      <section>
+        {allProducts.map(({ id, title, price }) => (
+          <article key={id}>
+            <span>{id}</span>
+            <p>{title}</p>
+            <span>{price}</span>
+          </article>
+        ))}
+      </section>
+    </>
+  );
 };
