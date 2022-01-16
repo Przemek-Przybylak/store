e4a0fcd9de0837451fcb6d4420302c9b71b69c6c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-16 22:20:23 +0100

    added fetchData in ProductsSaga

--- src/features/storePage/state/ProductsSaga.js
+++ src/features/storePage/state/ProductsSaga.js
@@ -1,8 +1,10 @@
 import { listSaga } from "../../../common/generatorSaga";
 import { actions } from "./ProductsSlice";
+import { fetchData } from "../../../common/fetchData";
 
 export function* watchFetchProducts() {
   yield listSaga({
     actions,
+    fetchData,
   });
 }
