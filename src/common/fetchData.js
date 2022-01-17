43219c85c7218574cf63b524a81497a5715dfea8
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-17 21:42:22 +0100

    added dynamic path in fetching function

--- src/common/fetchData.js
+++ src/common/fetchData.js
@@ -1,8 +1,12 @@
-export const fetchData = async () => {
-  const response = await fetch(`https://fakestoreapi.com/products/`);
-
+export const fetchData = async (path) => {
+  const response = await fetch(path);
+  console.log(response);
   if (!response.ok) {
     throw new Error(response.statusText);
   }
   return await response.json();
+};
+
+export const fetchProducts = async () => {
+  return await fetchData(`https://fakestoreapi.com/products/`);
 };
--- src/common/generatorSaga.js
+++ src/common/generatorSaga.js
@@ -1,11 +1,12 @@
 import { call, put, takeLatest, delay } from "redux-saga/effects";
 
-export function* listSaga({ actions, fetchData }) {
+export function* listSaga({ actions, fetchProducts }) {
   function* fecthListHandler() {
     yield delay(1000);
 
     try {
-      const response = yield call(fetchData);
+      const response = yield call(fetchProducts);
+      console.log(response);
       yield put(actions.fetchSuccess(response));
     } catch (error) {
       yield put(actions.fetchError());
