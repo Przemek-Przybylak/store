fa30b8003bf084830839fa280fd401c14bebfe12
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-01 17:41:39 +0200

    create generatorSlice

---
+++ src/common/generatorSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  status: "initial",
+};
+export const generatorSlice = ({ name }) => {
+  const slice = createSlice({
+    name,
+    initialState,
+    reducers: {
+      fetch: () => ({
+        status: "loading",
+      }),
+      fetchSuccess: (_, { payload }) => ({
+        products: payload,
+        status: "success",
+      }),
+      fetchError: () => ({
+        status: "error",
+      }),
+      clear: () => initialState,
+    },
+  });
+  return {
+    reducer: slice.reducer,
+    actions: slice.actions,
+    selectors: {
+      selectProducts: (state) => state[name].products,
+      selectStatus: (state) => state[name].status,
+    },
+  };
+};
